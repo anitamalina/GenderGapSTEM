80b49f4214466be2ba32d809c231eead523de15e
MLMeyer <meyer.marie.louise@gmail.com>
2022-04-05 12:31:43 +0200

    practise and play

--- ivis/src/App.js
+++ ivis/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import './myStyle.css';
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls, Stars} from "@react-three/drei";
+
+function Box(){
+  return (
+    <mesh>
+      <boxBufferGeometry attach="geometry"/>
+      <meshLambertMaterial attach="material" color="hotpink"/>
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-     <h1> Hello world!</h1>
-    </div>
+    <Canvas className='canvas'>
+      <OrbitControls/>
+      <Stars/>
+      <ambientLight intensity={0.5}/>
+      <spotLight angle={0.3} position={[10,15,10]}/>
+      <Box/>
+    </Canvas>
   );
 }
 
