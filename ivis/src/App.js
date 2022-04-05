ecf3a836186be07a9ecc575d84d6967309042236
anitamalina <anitatranberg@gmail.com>
2022-04-05 12:31:34 +0200

    pink box in space

--- ivis/src/App.js
+++ ivis/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import './style.css';
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stars } from '@react-three/drei'
+
+function Box() {
+  return (
+    <mesh>
+      <boxBufferGeometry attach="geometry"></boxBufferGeometry>
+      <meshLambertMaterial attach="material" color="hotpink"></meshLambertMaterial>
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-     <h1> Hello world!</h1>
-    </div>
+    <Canvas className="canvas">
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5}/>
+      <spotLight position={[10,15,10]} angle={0.3}/>
+      <Box/>
+    </Canvas>
   );
 }
 
