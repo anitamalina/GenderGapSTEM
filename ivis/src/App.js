75e430717e0ca6b63e5cebad53b58d51f000e45d
anitamalina <anitatranberg@gmail.com>
2022-04-06 18:15:18 +0200

    two colors sphere

--- ivis/src/App.js
+++ ivis/src/App.js
@@ -1,7 +1,7 @@
 import './style.css';
-import { Canvas, useLoader } from '@react-three/fiber'
+import { Canvas} from '@react-three/fiber'
 import { OrbitControls, Stars } from '@react-three/drei'
-import React, { useState, useCallback} from 'react'
+import React, { useState, useCallback } from 'react'
 
 function useHover(stopPropagation = true) {
   const [hovered, setHover] = useState(false)
@@ -25,28 +25,36 @@ function Sphere({ position}) {
   let [bindHover, hovered] = useHover(false)
 
   const vs = `
-    varying vec3 v_Normal;
+  varying vec2 vUv;
 
-    void main() {
-      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-      v_Normal= normal;
-    }`
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+  }`
+
   const fs = `
-  varying vec3 v_Normal;
+    uniform vec3 c1;
+    uniform vec3 c2;
+    uniform vec3 c3;
+
+    varying vec2 vUv;
 
     void main() {
-      gl_FragColor = vec4(v_Normal, 1.0);
+      gl_FragColor = vec4(mix(c1, c2, vUv.y), 1.0);
+
   }`
 
 
   return (
     <>
-    <ambientLight intensity={0.5}/>
+    <ambientLight intensity={0.1}/>
     <spotLight position={[-10,10,1]} angle={0.3}/>
     <mesh position={ position} {...bindHover} onClick={e => console.log(e)}>
       <sphereBufferGeometry args={[1.5, 30, 30]} attach="geometry" />
       <shaderMaterial
         attach="material"
+        wireframe="true"
+        uniforms={{ c1: {value: [0.995,0.575,0.829]}, c2: {value: [0.010,0.995,0.976] }}}
         vertexShader={vs}
         fragmentShader={fs}/>
       {/* <meshLambertMaterial attach="material" color={hovered ? 'black' : 'hotpink'}></meshLambertMaterial> */}
