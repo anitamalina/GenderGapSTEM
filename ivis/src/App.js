7accc0f20c76cd1289887dab0745c6cce926f19a
anitamalina <anitatranberg@gmail.com>
2022-04-06 11:01:14 +0200

    3 circles on a row

--- ivis/src/App.js
+++ ivis/src/App.js
@@ -1,24 +1,50 @@
 import './style.css';
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stars } from '@react-three/drei'
+import React, { useState, useCallback } from 'react'
 
-function Box() {
+function useHover(stopPropagation = true) {
+  const [hovered, setHover] = useState(false)
+  
+  const hover = useCallback(e => {
+    if (stopPropagation) e.stopPropagation()
+    setHover(true)
+  }, [])
+  
+  const unhover = useCallback(e => {
+    if (stopPropagation) e.stopPropagation()
+    setHover(false)
+  }, [])
+
+  const [bind] = useState(() => ({ onPointerOver: hover, onPointerOut: unhover }))
+  
+  return [bind, hovered]
+}
+
+function Sphere({ position }) {
+  let [bindHover, hovered] = useHover(false)
+
   return (
-    <mesh>
-      <boxBufferGeometry attach="geometry"></boxBufferGeometry>
-      <meshLambertMaterial attach="material" color="hotpink"></meshLambertMaterial>
+    <>
+    <ambientLight intensity={0.5}/>
+    <spotLight position={[-10,10,1]} angle={0.3}/>
+    <mesh position={ position} {...bindHover} onClick={e => console.log(e)}>
+      <sphereBufferGeometry args={[0.5, 30, 30]} attach="geometry" />
+      <meshLambertMaterial attach="material" color={hovered ? 'black' : 'hotpink'}></meshLambertMaterial>
     </mesh>
+    </>
   )
 }
+
 
 function App() {
   return (
     <Canvas className="canvas">
       <OrbitControls />
       <Stars />
-      <ambientLight intensity={0.5}/>
-      <spotLight position={[10,15,10]} angle={0.3}/>
-      <Box/>
+      <Sphere position={[2,0,0]} />
+      <Sphere position={[0,0,0]} />
+      <Sphere position={[-2,0,0]} />
     </Canvas>
   );
 }
