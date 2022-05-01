6e3f0553eb940d18b48856569f4c12b0b4720924
anitamalina <anitatranberg@gmail.com>
2022-05-01 16:30:13 +0200

    timer added

--- ivis/src/App.js
+++ ivis/src/App.js
@@ -3,12 +3,14 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
 import React, { useState } from "react";
 import Visuals from "./Visuals";
+import Timer from "./Timer";
 
 import Sphere from "./Sphere";
 import UpdateGenderBtn from "./UpdateGenderBtn";
 
 function App() {
   const [src, setSrc] = useState();
+  const [timer, setTimer] = useState();
 
   function goToPage() {
     return null;
@@ -16,9 +18,12 @@ function App() {
       
   return (
     <>
+    <h1>Student Representation</h1>
     <UpdateGenderBtn action={goToPage}/>
+    <p>{timer}</p>
       <div className="p5-sketch">
         <Visuals setSrc={setSrc}/>
+        <Timer setTimer={setTimer}/>
       </div>
     <Canvas className="canvas" >
       <OrbitControls />
