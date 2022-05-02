c7436c1375150d98c0362a7d94e47d1328365a3f
anitamalina <anitatranberg@gmail.com>
2022-05-02 10:20:52 +0200

    start on something new

--- ivis/src/controlboards/FlowBtn.js
+++ ivis/src/controlboards/FlowBtn.js
@@ -2,7 +2,7 @@ import './../myStyle.css'
 
 export default function flowBtn({txt, action}) {
     return (
-        <button className="flowBtn"onClick={action}>
+        <button className="flowBtn" onClick={action}>
         <p className="flowBtnTxt">{txt}</p>
         </button>
     )
