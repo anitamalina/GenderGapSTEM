ab7d516e8837664f73a1b9f4475835919eb401ab
anitamalina <anitatranberg@gmail.com>
2022-05-04 08:14:39 +0200

    no routing - deactivate useState works

--- ivis/src/App.js
+++ ivis/src/App.js
@@ -1,52 +1,19 @@
 import "./myStyle.css";
 import React, { useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stars } from "@react-three/drei";
 
-import data from "./data.json";
+import Home from "./pages/Home";
 
-import Timer from "./components/Timer";
-import GenderInfo from "./components/GenderInfo";
-import UpdateGenderBtn from "./components/UpdateGenderBtn";
-import Visuals from "./components/Visuals";
-import Sphere from "./components/Sphere";
 
-function App() {
-  const [src, setSrc] = useState();
-  const [timer, setTimer] = useState();
 
-  function goToPage() {
-    return null;
-  }
+export default function App() {
+
+/*   const [question1, setQuestion1] = useState
+  const [question2, setQuestion2] = useState
+  const [questionConfirm, setQuestionConfirm] = useState */
 
   return (
     <>
-      <Timer setTimer={setTimer} />
-      <h1>Student Representation</h1>
-      <div className="genderInfo">
-        {data.map((g) => (
-          <div className="genderText">
-            <GenderInfo
-              genderText={g.genderText}
-              genderPercent={g.genderPercent + " %"}
-              genderColor={g.genderColor}
-            />
-          </div>
-        ))}
-      </div>
-      <UpdateGenderBtn action={goToPage} />
-
-      <p className="timer">{timer}</p>
-      <div className="p5-sketch">
-        <Visuals setSrc={setSrc} />
-      </div>
-      <Canvas className="canvas">
-        <OrbitControls />
-        <Stars />
-        <Sphere position={[0, 0, 0]} src={src} />
-      </Canvas>
+      <Home />
     </>
   );
 }
-
-export default App;
---
+++ ivis/src/pages/Home.js
@@ -0,0 +1,51 @@
+import "./../myStyle.css"
+import React, { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
+
+import data from "./../data.json"
+
+import Timer from "../components/Timer";
+import GenderInfo from "../components/GenderInfo";
+import UpdateGenderBtn from "../components/UpdateGenderBtn";
+import Visuals from "../components/Visuals";
+import Sphere from "../components/Sphere";
+
+
+export default function Home() {
+    const [src, setSrc] = useState();
+    const [timer, setTimer] = useState();
+
+  function showQuestion1() {
+    console.log("btn clickd!! ")
+  }
+
+  return (
+    <>
+      <Timer setTimer={setTimer} />
+      <h1>Student Representation</h1>
+      <div className="genderInfo">
+        {data.map((g) => (
+          <div className="genderText">
+            <GenderInfo
+              genderText={g.genderText}
+              genderPercent={g.genderPercent + " %"}
+              genderColor={g.genderColor}
+            />
+          </div>
+        ))}
+      </div>
+      <UpdateGenderBtn action={showQuestion1} />
+
+      <p className="timer">{timer}</p>
+      <div className="p5-sketch">
+        <Visuals setSrc={setSrc} />
+      </div>
+      <Canvas className="canvas">
+        <OrbitControls />
+        <Stars />
+        <Sphere position={[0, 0, 0]} src={src} />
+      </Canvas>
+    </>
+  );
+}
