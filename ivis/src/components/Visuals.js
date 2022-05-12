0d03548c9f967f68d44129a1b0efbc7b92ba4ace
MLMeyer <meyer.marie.louise@gmail.com>
2022-05-12 14:33:03 +0200

    updated direction of colors in p5 canvas

--- ivis/src/components/Visuals.js
+++ ivis/src/components/Visuals.js
@@ -3,18 +3,17 @@ import Sketch from "react-p5";
 
 import { getPercentage } from "../percentage";
 
-export default function Visuals({setSrc, data}) {
-
+export default function Visuals({ setSrc, data }) {
   const setup = (p5, canvasParentRef, canvas) => {
     p5.createCanvas(600, 400).parent(canvasParentRef);
   };
 
-  function destructure(input){
+  function destructure(input) {
     let percentage = getPercentage(input, data);
-    let genderSquare= {
-      rectHeight: (400*percentage)/100,
-      color: input.color
-    }
+    let genderSquare = {
+      rectWidth: (600 * percentage) / 100,
+      color: input.color,
+    };
     return genderSquare;
   }
 
@@ -23,27 +22,24 @@ export default function Visuals({setSrc, data}) {
   const draw = (p5) => {
     p5.background(220);
     //let numbersOfGenders = [25, 50];
-    console.log("From Visuals", squareArray)
-    let sumHeight = 0;
+    console.log("From Visuals", squareArray);
+    let sumWidth = 0;
     for (let i = 0; i < squareArray.length; i++) {
-
       if (i === 0) {
         p5.fill(squareArray[i].color);
         p5.noStroke();
-        p5.rect(0, 0, 600, squareArray[i].rectHeight);
-        sumHeight += squareArray[i].rectHeight;
-        
+        p5.rect(0, 0, squareArray[i].rectWidth, 400);
+        sumWidth += squareArray[i].rectWidth;
       } else {
         p5.fill(squareArray[i].color);
-        p5.rect(0, sumHeight, 600, squareArray[i].rectHeight);
-        sumHeight += squareArray[i].rectHeight;
-
+        p5.rect(sumWidth, 0, squareArray[i].rectWidth, 400);
+        sumWidth += squareArray[i].rectWidth;
       }
     }
-    
+
     const imgSrc = p5.canvas.toDataURL();
     setSrc(imgSrc);
     p5.canvas.hide();
-  }
-  return <Sketch setup={setup} draw={draw}/>;
-};
+  };
+  return <Sketch setup={setup} draw={draw} />;
+}
